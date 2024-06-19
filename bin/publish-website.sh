ab6a0d717becbc813849590896eac459df6af3af
Ikaros Kappler <info@int2byte.de>
2024-06-20 00:26:38 +0200

    Minor changes.

---
+++ bin/publish-website.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+
+if [[ -z "$user" && -z "$server" && -z "$destination" ]]; then
+    echo "No env vars found. Loading up .env file ..."
+    set -o allexport
+    [[ -f ../.env ]] && source ../.env
+    set +o allexport
+else
+    echo "Env vars found."
+fi
+
+
+while true; do
+    read -p "Do you wish compile the hugo pages before uploading (y/n)? " yn
+    case $yn in
+        [Yy]* ) cd ../ && npm run bild && cd ../bin; break;;
+        [Nn]* ) break;; #exit;;
+        * ) echo "Please answer y or n.";;
+    esac
+done
+
+
+
+echo "Uploading to $server ..."
+
+# rsync -avH ../docs_jekyll/_site/*  -e ssh user@server:/your/destination/path
+# rsync -avH ../docs_jekyll/_site/*  -e ssh $user@$server:"$destination"
+# rsync -avH ../demos_with_tracker/* -e ssh $user@$server:"$destination/repo/demos/"
+# rsync -avH ../dist                 -e ssh $user@$server:"$destination/repo/"
+# rsync -avH ../lib                  -e ssh $user@$server:"$destination/repo/"
+# rsync -avH ../screenshots          -e ssh $user@$server:"$destination/repo/"
+# rsync -avH ../src                  -e ssh $user@$server:"$destination/repo/"
+# rsync -avH ../tests                -e ssh $user@$server:"$destination/repo/"
+# rsync -avH ../*.*                  -e ssh $user@$server:"$destination/repo/"
+# rsync -avH ../releases/*           -e ssh $user@$server:"$destination/releases/"
+
+echo "destination $destination"
+rsync -avH ../public/*  -e ssh $user@$server:"$destination"
+
